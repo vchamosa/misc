f7421de9f8b01a3eb2f5f8b4072fdf2d0a297b79
vchamosa <37210240+vchamosa@users.noreply.github.com>
2023-02-28 23:16:13 +0000

    added functionality for swap chance no

--- monty_hall.R
+++ monty_hall.R
@@ -17,22 +17,19 @@
 #
 # This simulator lets you play as many trials of the game as you'd like and reports the
 # results, to show that indeed the better strategy is to switch to the remaining door.
-# It also lets you play around with the number of doors, though only one switch will be
-# permitted per trial.
+# It also lets you play around with the number of doors and chances to swap.
 #
 
 
 ## Set up simulation
 options(scipen = 999)
 
-runs <- 100000 # number of simulation trials
+runs <- 1000 # number of simulation trials
 doors <- 3 # number of doors (minimum and most famously 3)
+chances <- doors - 2 # number of chances to switch doors
 
 prize <- sample(1:doors, runs, replace = TRUE) # select the winning door for every trial
-firstchoice <- sample(1:doors, runs, replace = TRUE) # select the contestant's first choice of door for every trial
-
-revealdoor <- rep(0,runs) # initialise variable for door to be revealed after first choice made
-swap <- rep(0,runs) # initialise variable for door to swap to
+choice <- sample(1:doors, runs, replace = TRUE) # select the contestant's first choice of door for every trial
 
 stay_wins <- 0 # initialise variable for number of wins when sticking to first choice
 swap_wins <- 0 # initialise variable for number of wins when switching doors
@@ -41,15 +38,21 @@ swap_wins <- 0 # initialise variable for number of wins when switching doors
 ## Run game simulation
 for(i in 1:runs){
 #staying strategy
-  if(firstchoice[i] == prize[i]){
+  if(choice[i] == prize[i]){
     stay_wins <- stay_wins + 1
   }
 
 #switching strategy
-  remainder <- c(1:doors)[-c(prize[i], firstchoice[i])]
-  revealdoor <- remainder[sample(length(remainder), 1)]
-  swap <- c(1:doors)[-c(revealdoor, firstchoice[i])]
-  swap <- swap[sample(length(swap), 1)]
+  options <- c(1:doors) # total set of doors
+  remainder <- options[-c(prize[i], choice[i])] # subset of doors the host can reveal
+  swap <- choice[i]
+  for(sdt in 1:chances){
+    revealdoor <- remainder[sample(length(remainder), 1)] # pick door to be revealed after a choice is made
+    swap <- options[! options %in% c(revealdoor, swap)] # pick door to switch to
+    swap <- swap[sample(length(swap), 1)]
+    options <- options[! options %in% c(revealdoor)]
+    remainder <- remainder[! remainder %in% c(revealdoor)]
+  }
   if(swap == prize[i]){
     swap_wins <- swap_wins + 1
   }
@@ -58,6 +61,6 @@ for(i in 1:runs){
 
 ## Display simulation results
 print(paste0("Sticking to the original door choice won ", stay_wins, " out of ", runs," trials."))
-print(paste0("Switching to the other door won ", swap_wins, " out of ", runs," trials."))
+print(paste0("Switching doors won ", swap_wins, " out of ", runs," trials."))
 
 
